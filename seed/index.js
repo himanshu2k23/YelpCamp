c2fba5bf5e972c0a27b0036f2c7b8e6995871084
himanshu2k23 <himanshudubey2k23@gmail.com>
2023-04-11 23:29:06 +0530

    styles and model changes

--- seed/index.js
+++ seed/index.js
@@ -26,11 +26,14 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
     await Campground.deleteMany({});
-    for (let i = 0; i < 20; i++) {
+    for (let i = 0; i < 10; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            img:'http://source.unsplash.com/collection/483251',
+            price: Math.floor(Math.random() * 20),
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sollicitudin, sem vitae varius tincidunt, augue sapien tempor dui, a vulputate nisl risus ac nisl. Integer id eros eu mi interdum suscipit sed quis risus. Sed pharetra, enim eu gravida lacinia, neque odio accumsan mauris, nec aliquam lectus mauris et lectus. Praesent blandit enim eu luctus faucibus. Nulla facilisi. Sed vestibulum justo vitae sapien vestibulum, a ultrices leo fringilla. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Mauris at felis lectus. Nulla facilisi. Sed vel est ac tellus ullamcorper ultrices. Sed interdum augue nec leo aliquet, id tincidunt magna convallis. Suspendisse eget est quis turpis facilisis volutpat.'
         })
         await camp.save();
     }
