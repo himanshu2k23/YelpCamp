4389d6a0bff97f1a7f1f758c732b7033d9e066f8
himanshu2k23 <himanshudubey2k23@gmail.com>
2023-04-13 10:11:47 +0530

    error handling

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ const app=express();
 const path= require('path');
 const mongoose=require('mongoose')
 const methodOverride=require('method-override');
-const Campground=require(path.join(__dirname,'/models/campground'))
+const Campground=require(path.join(__dirname,'/models/campground'));
+const ExpressError=require('./utils/ExpressError');
+const catchAsync=require('./utils/catchAsync');
 const engine=require('ejs-mate');
 //CONNECTING DATABASE
+console.log({catchAsync})
 console.log(main());
 async function main() {
     await mongoose.connect('mongodb+srv://him:himanshu@cluster0.yfpx5hl.mongodb.net/?retryWrites=true&w=majority')
@@ -29,52 +32,53 @@ app.use(express.urlencoded({extends:true}))
 
 
 //INDEX
-app.get('/campgrounds', async (req,res)=>{
+app.get('/campgrounds', catchAsync( async (req,res)=>{
     const campgrounds=await Campground.find({});
     res.render(path.join(__dirname,'views/campground/index.ejs'),{campgrounds,pageTitle:"Index"})
-})
+}))
 
 //POST
 app.get('/campgrounds/new', (req,res)=>{
     res.render(path.join(__dirname,'views/campground/new.ejs'),{pageTitle:"Add"})
 })
-app.post('/campgrounds', async (req,res)=>{
+app.post('/campgrounds', catchAsync(async (req,res)=>{
     //console.log(req.body)
     const newCampground=new Campground(req.body);
     await newCampground.save();
     res.redirect(`/campgrounds/${newCampground._id}`);
-})
+}))
 
 //DETAILS
-app.get('/campgrounds/:id', async (req,res)=>{
-    try {
-        const {id}=req.params;
-        const campgrounds=await Campground.findById(id);
-        res.render(path.join(__dirname,'views/campground/details.ejs'),{campgrounds,pageTitle:"Details"})
-    } catch (error) {
-        console.log(error);
-    } 
-})
+app.get('/campgrounds/:id', catchAsync(async (req,res)=>{
+    const {id}=req.params;
+    const campgrounds=await Campground.findById(id);
+    res.render(path.join(__dirname,'views/campground/details.ejs'),{campgrounds,pageTitle:"Details"}) 
+}))
 
 //EDIT 
-app.get('/campgrounds/:id/edit', async (req,res)=>{
+app.get('/campgrounds/:id/edit', catchAsync(async (req,res,next)=>{
     const {id}=req.params;
     const campgrounds=await Campground.findById(id);
     res.render(path.join(__dirname,'views/campground/edit.ejs'),{campgrounds,pageTitle:"Edit"})
-})
-app.patch('/campgrounds/:id/edit', async (req,res)=>{
+}))
+app.patch('/campgrounds/:id/edit', catchAsync(async (req,res)=>{
     //console.log(req.body)
     const {id}=req.params;
     const newCampground=await Campground.findByIdAndUpdate(id,req.body);
     res.redirect(`/campgrounds/${newCampground._id}`);
-})
+}))
 
 //DELETE
-app.delete('/campgrounds/:id/delete', async (req,res)=>{
+app.delete('/campgrounds/:id/delete', catchAsync(async (req,res)=>{
     //console.log(req.body)
     const {id}=req.params;
     const newCampground=await Campground.findByIdAndDelete(id);
     res.redirect(`/campgrounds`);
+}))
+
+//ERROR HANDLER
+app.use((err,req,res,next)=>{
+    res.send("error")
 })
 
 
