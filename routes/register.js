1031164ceadf72475bc9795654132e44591fb987
himanshu2k23 <himanshudubey2k23@gmail.com>
2023-04-21 00:33:13 +0530

    register error

--- routes/register.js
+++ routes/register.js
@@ -2,20 +2,26 @@ const express = require('express');
 const router = express.Router();
 const ExpressError = require('../utils/ExpressError');
 const User = require('../models/user');
-const {userSchema} =require('../schema');
+const { userSchema } = require('../schema');
 const catchAsync = require('../utils/catchAsync');
 const flash = require('connect-flash');
 
-router.get('/',(req,res)=>{
-    res.render('../views/campground/register', { pageTitle: "Add"})
+router.get('/', (req, res) => {
+    res.render('../views/campground/register', { pageTitle: "Add" })
 })
-router.post('/', catchAsync(async(req,res)=>{
-    const {username,email,password}=req.body;
-    const user=new User({username,email});
-    const registerUser=await User.register(user,password);
-    //console.log({registerUser});
-    req.flash('success','Welcome to yelp camp');
-    res.redirect('/campgrounds');
+router.post('/', catchAsync(async (req, res) => {
+    try {
+        const { username, email, password } = req.body;
+        const user = new User({ username, email });
+        const registerUser = await User.register(user, password);
+        //console.log({registerUser});
+        req.flash('success', 'Welcome to yelp camp');
+        res.redirect('/campgrounds');
+    }
+    catch (error) {
+        req.flash('error', error.message);
+        res.redirect('/register');
+    }
 }))
 
-module.exports=router;
+module.exports = router;
