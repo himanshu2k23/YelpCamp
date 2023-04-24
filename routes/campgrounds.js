b43de1659ad00abbf88551b5903e14be32aaf8cb
himanshu2k23 <himanshudubey2k23@gmail.com>
2023-04-24 20:31:16 +0530

    review auth

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -30,7 +30,7 @@ router.post('',isLoggedIn,validateCampground, catchAsync(async (req, res) => {
 router.get('/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findById(id).populate({path:'reviews',populate:{ path:'author'}}).populate('author');
-    console.log(campground);
+    //console.log(campground);
     if (!campground) {
         req.flash('error','Campground not found');
         res.redirect('/campgrounds')        
