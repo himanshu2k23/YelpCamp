e167e57aa54d2d11f0e6d8fa06e197dd867ba576
himanshu2k23 <himanshudubey2k23@gmail.com>
2023-04-29 21:03:28 +0530

    model change

--- models/campground.js
+++ models/campground.js
@@ -1,35 +1,40 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const Review =require('./review');
+const Review = require('./review');
 const review = require('./review');
 
 const CampgroundSchema = new Schema({
     title: String,
-    img:String,
+    img: [
+        {
+            url: String,
+            name:String
+        }
+    ],
     price: Number,
     description: String,
-    location:String,
-    author:{
-        type:Schema.Types.ObjectId,
-        ref:'User'
+    location: String,
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
     },
-    reviews:[
+    reviews: [
         {
-            type:Schema.Types.ObjectId,
-            ref:'Review'
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
         }
     ]
 
 })
 
-CampgroundSchema.post('findOneAndDelete',async(doc)=>{
+CampgroundSchema.post('findOneAndDelete', async (doc) => {
     if (doc) {
         await Review.deleteMany({
-            _id:{
+            _id: {
                 $in: doc.reviews
             }
         })
     }
 })
 
-module.exports = mongoose.model('Campground',CampgroundSchema)
+module.exports = mongoose.model('Campground', CampgroundSchema)
