27925ae27c87db75d8c05bc538fe661777663893
himanshu2k23 <himanshudubey2k23@gmail.com>
2023-04-30 21:14:43 +0530

    seed fix

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -1,4 +1,4 @@
-require('dotenv').config('../');
+
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
--- seed/index.js
+++ seed/index.js
@@ -1,5 +1,5 @@
-//VARIABLE DECLARATION
-require('dotenv').config('../');
+
+require('dotenv').config({path: '../.env'});
 const express = require('express');
 const app = express();
 const path = require('path');
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
 const { title } = require('process');
 const cities = require(path.join(__dirname,'/cities.js'));
 const { places,descriptors }= require(path.join(__dirname,'/seedhelpers.js'));
-//console.log({places,descriptors});
+//console.log(process.env.MONGODB_URI);
 const Campground = require(path.join(__dirname, '../models/campground'));
 //CONNECTING DATABASE
 mongoose.set('strictQuery', false);
@@ -34,7 +34,17 @@ const seedDB = async () => {
             author:'64439d9f1300686e0f15c1fb',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            img:'http://source.unsplash.com/collection/483251',
+            images:[
+                {
+                  url: 'https://res.cloudinary.com/dtgiodduq/image/upload/v1682785185/YelpCamp/stvvnglpms2ngdmxaqm1.avif',
+                  name:'stvvnglpms2ngdmxaqm1.avif'
+                
+                },
+                {
+                  url: 'https://res.cloudinary.com/dtgiodduq/image/upload/v1682785187/YelpCamp/nbdbnep3nw6caig2bb53.avif',
+                  name:'nbdbnep3nw6caig2bb53.avif'
+                }
+            ],
             price: Math.floor(Math.random() * 20),
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sollicitudin, sem vitae varius tincidunt, augue sapien tempor dui, a vulputate nisl risus ac nisl. Integer id eros eu mi interdum suscipit sed quis risus. Sed pharetra, enim eu gravida lacinia, neque odio accumsan mauris, nec aliquam lectus mauris et lectus. Praesent blandit enim eu luctus faucibus. Nulla facilisi. Sed vestibulum justo vitae sapien vestibulum, a ultrices leo fringilla. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Mauris at felis lectus. Nulla facilisi. Sed vel est ac tellus ullamcorper ultrices. Sed interdum augue nec leo aliquet, id tincidunt magna convallis. Suspendisse eget est quis turpis facilisis volutpat.'
         })
